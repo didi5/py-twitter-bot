ca1d22aa110d18114f62ee5fd1bcaf723a23529b
didi5 <39310464+didi5@users.noreply.github.com>
2018-05-15 19:05:57 +0200

    Änderung
    Sendet jetzt ein anderen Text.

--- tweet_text.py
+++ tweet_text.py
@@ -20,5 +20,5 @@ def idle_text():
     # Construct the text we want to tweet out (280 chars max)
     berlin_time = datetime.now(timezone('Europe/Berlin'))
     formatted_time = berlin_time.strftime("%H:%M:%S on a %A")
-    text = "It is " + formatted_time + "!"
+    text = "Hallo HPI! Dies ist der Bot TweetBremse " #+ formatted_time + "!"
     return text
